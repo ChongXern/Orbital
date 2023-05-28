8574e9ebc3eb9f2f77777f07b8561f93cb4a9dad
hongyijie06 <110654840+hongyijie06@users.noreply.github.com>
2023-05-28 18:18:19 +0800

    Add files via upload

---
+++ Godot/ally.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+signal hit
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+
+
+
+func _on_area_2d_body_entered(body):
+	hit.emit()
