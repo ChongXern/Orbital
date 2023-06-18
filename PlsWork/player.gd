6b2d00105521d43532f2d2876086ae8226047cd4
ChongXern <chongxern@gmail.com>
2023-06-18 13:57:43 +0800

    new commit

--- PlsWork/player.gd
+++ PlsWork/player.gd
@@ -1,48 +1,57 @@
 extends CharacterBody2D
+
 @export var speed = 400
 var screen_size
+var current_dir = "none"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite2D.play("running")
 	screen_size = get_viewport_rect().size
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO
-	#control movement of player based on input
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
 	
-	#restrict movement of player to within frame bounds
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
 	
+	'''
 	if velocity.x >= 0:
 		$AnimatedSprite2D.flip_h = true
 	else:
-		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.flip_h = false'''
+
+func compute_animation(current_dir):
+	var anim = $AnimatedSprite2D
+	anim.play("running")
+	if current_dir == "right":
+		anim.flip_h = true
+	else:
+		anim.flip_h = false
 
 func handleInput():
 	var moveDirection = Input.get_vector("left", "right", "up", "down")
 	velocity = moveDirection * speed
 
-#handles interaction between player and assets
 func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("right"):
+		current_dir = "right"
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		current_dir = "left"
+		velocity.x -= 1
+	compute_animation(current_dir)
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	# else:
+		# $AnimatedSprite2D.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 	handleInput()
 	move_and_slide()
-
-
+	
